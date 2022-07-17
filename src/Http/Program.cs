3b492cdc4ef9f56e7d883b2955b403b9fa69b1ee
loxloxloxlox <senja2018@mail.ru>
2022-07-17 22:23:12 +0300

    The output of http headers to the console has been added

--- src/Http/Program.cs
+++ src/Http/Program.cs
@@ -1,8 +1,56 @@
-﻿using (var client = new HttpClient())
-{
-    var url = args[0];
-    
-    var msg = await client.GetAsync(url);
+﻿var argsLength = args.Length;
 
-    Console.WriteLine(await msg.Content.ReadAsStringAsync());
+if (argsLength == 0) Console.Error.WriteLine("Error: no arguments");
+
+using (var client = new HttpClient())
+{
+    try
+    {
+        var url = "";
+
+        if (argsLength == 1)
+        {
+            url = args[0];
+
+            var msg = await client.GetAsync(url);
+
+            Print("!----CONTENT----!");
+
+            Console.WriteLine(await msg.Content.ReadAsStringAsync());
+
+            Print("!----HEADERS----!");
+
+            foreach (var header in msg.Headers)
+            {
+                Console.WriteLine($"{header.Key}: {header.Value.First()}");
+            }
+        }
+
+        //if(args.Length == 2 && args[1].ToLower() == "post")
+        //{
+        //    url = args[0];
+
+        //    var msg = await client.PostAsync(url, )
+
+        //}
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.Error.WriteLine(ex.Message);
+    }
 }
+
+#region Methods
+
+void Print(string text)
+{
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine(text);
+    Console.ResetColor();
+    Console.WriteLine();
+}
+
+#endregion
