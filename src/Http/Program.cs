963f0459cc02afac7136d06be7d1c9c979156100
loxloxloxlox <senja2018@mail.ru>
2022-07-17 20:12:17 +0300

    Добавьте файлы проекта.

---
+++ src/Http/Program.cs
@@ -0,0 +1,8 @@
+﻿using (var client = new HttpClient())
+{
+    var url = args[0];
+    
+    var msg = await client.GetAsync(url);
+
+    Console.WriteLine(await msg.Content.ReadAsStringAsync());
+}
